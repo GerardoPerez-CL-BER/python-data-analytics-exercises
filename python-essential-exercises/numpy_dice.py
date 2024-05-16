b881becd1b39d6ca70693cdd46926e3dc43746cc
Gerardo Pérez <yerko.pg@gmail.com>
2024-05-16 15:25:49 +0200

    Update numpy_dice.py

--- python-essential-exercises/numpy_dice.py
+++ python-essential-exercises/numpy_dice.py
@@ -1,6 +1,7 @@
 # Import numpy as np
 import numpy as np 
 import time
+import matplotlib.pyplot as plt
 
 # Set the dynamic seed
 np.random.seed(int(time.time()))
@@ -25,3 +26,26 @@ else :
 # Print out dice and step
 print(dice)
 print(step)
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
+
+# Plot random_walk
+plt.plot(random_walk)
+plt.show()
