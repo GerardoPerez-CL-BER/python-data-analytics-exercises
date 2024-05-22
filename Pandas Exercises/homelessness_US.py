e73522a15fbaa08dff8b2894e355c6dfc01c3a59
Gerardo Pérez <yerko.pg@gmail.com>
2024-05-22 23:47:21 +0200

    Update homelessness_US.py

--- Pandas Exercises/homelessness_US.py
+++ Pandas Exercises/homelessness_US.py
@@ -1,3 +1,43 @@
 import pandas as pd 
 
 homelessness = pd.read_csv("homelessness_us.csv")
+
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print the values of homelessness
+print(homelessness.values)
+
+# Print the column index of homelessness
+print(homelessness.columns)
+
+# Print the row index of homelessness
+print(homelessness.index)
+
+# Sort homelessness by individuals
+homelessness_ind = homelessness.sort_values("individuals")
+
+# Print the top few rows
+print(homelessness_ind.head())
+
+# Select the individuals column
+individuals = homelessness["individuals"]
+
+# Print the head of the result
+print(individuals.head())
+
+# Filter for rows where family_members is less than 1000 
+# and region is Pacific
+fam_lt_1k_pac = homelessness[(homelessness["family_members"] < 1000) & (homelessness["region"] == "Pacific")]
+
+# See the result
+print(fam_lt_1k_pac)
+
+# The Mojave Desert states
+canu = ["California", "Arizona", "Nevada", "Utah"]
+
+# Filter for rows in the Mojave Desert states
+mojave_homelessness = homelessness[homelessness["state"].isin(canu)]
+
+# See the result
+print(mojave_homelessness)
